8e5a1b604c47521455c7366600e8cb61f3945ffe
robomani <robomani@hotmail.com>
2018-05-28 11:10:08 -0400

    Start

---
+++ Prog2DTP1/Assets/Scripts/TextManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextManager : MonoBehaviour
+{
+    [SerializeField]
+    private string m_Intro;
+    [SerializeField]
+    private TextMeshProUGUI m_PlayerTextBox;
+	
+	void Start ()
+    {
+        m_PlayerTextBox.text = m_Intro;
+	}
+	
+
+	void Update ()
+    {
+		
+	}
+}
